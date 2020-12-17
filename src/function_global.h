c4521646250cfb9465df363a58110fee11d118a6
BRUN ALOIS p1706811 <alois.brun@etu.univ-lyon1.fr>
2020-12-17 22:40:46 +0100

    init function_global

--- src/function_global.h
+++ src/function_global.h
@@ -1,6 +1,32 @@
+/***********************************************
+      FICHIER DE FONCTIONS A USAGE GLOBAL
+*************************************************/
+
 #ifndef FUNC_GLOBAL_H_INCLUDED
 #define FUNC_GLOBAL_H_INCLUDED
 
+#include "types.h"
+#include <math.h>
+#include <vector>
+
+
+float norme(vertex N);
+
+float fast_inverse_square_root(float x);
+
+float angleEntreVec3(vertex AB, vertex AC);
+
+vertex getNormal(vertex AB, vertex AC);
+
+
+///         A JETER DES QUE POSSIBLE
+void pop_at(std::vector<int> &V, unsigned int H);
+
+void push_at(std::vector<int> &V, int I, unsigned int H);
+
+void pop_at(std::vector<Vec2> &V, unsigned int H);
+
+void push_at(std::vector<Vec2> &V, Vec2 I, unsigned int H);
 
 
 #endif // FUNC_GLOBAL_H_INCLUDED
--- src/main_geometrie.cpp
+++ src/main_geometrie.cpp
@@ -11,6 +11,7 @@
 
 #include "const.h"
 #include "types.h"
+#include "function_global.h"
 
 
 
@@ -19,38 +20,7 @@
 
 
 
-float norme(vertex N){
-    return sqrt(N.x*N.x + N.y*N.y + N.z*N.z);
-}
 
-float fast_inverse_square_root(float x){
-    float xhalf = 0.5f * x;
-    int i = *(int *)&x;
-    i = 0x5f3759df - (i >> 1);
-    x = *(float *)&i;
-    x = x*(1.5f - xhalf*x*x);
-
-    return x;
-}
-
-float angleEntreVec3(vertex AB, vertex AC){
-        ///AC.AB = ||AC||.||AB||.cos(theta) <=> cos(theta) = AC.AB / (||AC||.||AB||)
-        return acos((AC.x*AB.x + AC.y*AB.y + AC.z*AB.z)
-                        * fast_inverse_square_root(AB.x*AB.x + AB.y*AB.y + AB.z*AB.z)       ///on utilise la fonction de calcul rapide pour la racine inversé
-                            * fast_inverse_square_root(AC.x*AC.x + AC.y*AC.y + AC.z*AC.z));
-}
-
-vertex getNormal(vertex AB, vertex AC){
-    vertex N;
-
-    N.x = AB.y*AC.z - AB.z*AC.y;
-    N.y = AB.z*AC.x - AB.x*AC.z;
-    N.z = AB.x*AC.y - AB.y*AC.x;
-
-    N = N * fast_inverse_square_root(N.x*N.x + N.y*N.y + N.z*N.z);
-
-    return -N;
-}
 
 
 
@@ -485,35 +455,7 @@ int returnZone(Vec2 v){
 
 
 
-void pop_at(std::vector<int> &V, unsigned int H){
-    for(unsigned int i = H; i < V.size() - 1; i++){
-        V[i] = V[i+1];
-    }
-    V.pop_back();
-}
 
-void push_at(std::vector<int> &V, int I, unsigned int H){
-    V.push_back(I);
-    for(unsigned int i = V.size() - 1; i > H + 1; i--){
-        V[i] = V[i-1];
-    }
-    V[H + 1] = I;
-}
-
-void pop_at(std::vector<Vec2> &V, unsigned int H){
-    for(unsigned int i = H; i < V.size() - 1; i++){
-        V[i] = V[i+1];
-    }
-    V.pop_back();
-}
-
-void push_at(std::vector<Vec2> &V, Vec2 I, unsigned int H){
-    V.push_back(I);
-    for(unsigned int i = V.size() - 1; i > H + 1; i--){
-        V[i] = V[i-1];
-    }
-    V[H + 1] = I;
-}
 
 void interpolateY(std::vector<Vec2> &V, Vec2 S){
     Vec2 SV = V[V.size() - 1];
