b747bfdcd0fcf9f10ba0680baeb96998dd076318
BRUN ALOIS p1706811 <alois.brun@etu.univ-lyon1.fr>
2020-12-18 01:07:58 +0100

    init myTexture

--- src/main_geometrie.cpp
+++ src/main_geometrie.cpp
@@ -14,6 +14,7 @@
 #include "function_global.h"
 #include "camera.h"
 #include "player.h"
+#include "myTexture.h"
 
 
 
@@ -21,116 +22,7 @@
 
 
 
-class myTexture{
-private:
-    uint32_t ***tex;
-    int *w, *h,
-        nb_mipmap = 1;
 
-public:
-    void init(std::string file_name, SDL_Window *screen){
-        SDL_Surface *im = IMG_Load(file_name.c_str());
-
-        if(im == NULL)
-            std::cout << "erreur, impossible de charger l'image " << file_name << std::endl;
-
-        SDL_LockSurface(im);
-
-        int w2 = im->w,
-            h2 = im->h;
-        while(w2 != 1 && h2 != 1){
-            w2 = w2/2; ///(int)((float)w/2 + 0.5f);
-            h2 = h2/2; ///(int)((float)h/2 + 0.5f);
-            nb_mipmap++;
-        }
-        tex = new uint32_t**[nb_mipmap];
-        w = new int[nb_mipmap];
-        h = new int[nb_mipmap];
-
-
-        w2 = im->w;
-        h2 = im->h;
-        for(int i = 0; i < nb_mipmap; i++){
-            w[i] = w2;
-            h[i] = h2;
-
-            w2 = w2/2; ///(int)((float)w/2 + 0.5f);
-            h2 = h2/2; ///(int)((float)h/2 + 0.5f);
-        }
-
-
-        ///std::cout << (int)im->format->BitsPerPixel << std::endl;
-        uint8_t *pixel = 0;
-        uint8_t r = 0, g = 0, b = 0, a = 0;
-        tex[0] = new uint32_t*[w[0]];
-        for(int i = 0; i < w[0]; i++){
-            tex[0][i] = new uint32_t[h[0]];
-            for(int j = 0; j < h[0]; j++){
-                pixel = (uint8_t*)im->pixels + ((h[0] - 1 - j) * w[0] + i) * 3;      ///hautement non-optimal!!!
-                r = *pixel;
-                g = *(pixel + 1);
-                b = *(pixel + 2);
-                tex[0][i][j] = (uint32_t)((((((a << 8) + r) << 8) + g) << 8) + b);
-            }
-        }
-
-
-        for(int m = 1; m < nb_mipmap; m++){
-            tex[m] = new uint32_t*[w[m]];
-            for(int i = 0; i < w[m]; i++){
-                tex[m][i] = new uint32_t[h[m]];
-                for(int j = 0; j < h[m]; j++){
-                    uint32_t  Rm = 0,
-                              Gm = 0,
-                              Bm = 0;
-                    for(int k = 0; k < 2; k++){
-                        for(int l = 0; l < 2; l++){
-                            Rm += (unsigned char)(tex[m - 1][i*2 + k][j*2 + l] >> 16);
-                            Gm += (unsigned char)(tex[m - 1][i*2 + k][j*2 + l] >> 8);
-                            Bm += (unsigned char)tex[m - 1][i*2 + k][j*2 + l];
-                        }
-                    }
-
-                    Rm /= 4;
-                    Gm /= 4;
-                    Bm /= 4;
-                    tex[m][i][j] = (((Rm << 8) + Gm) << 8) + Bm;
-                }
-            }
-        }
-
-        SDL_FreeSurface(im);
-    }
-
-    int getMipMap(){
-        return nb_mipmap;
-    }
-
-    int getH(int i){
-        return h[i];
-    }
-
-    int getW(int i){
-        return w[i];
-    }
-
-    uint32_t getTex(int mm, int w, int h){
-        return tex[mm][w][h];
-    }
-
-    ~myTexture(){
-        for(int i = 0; i < nb_mipmap; i++){
-            for(int j = 0; j < w[i]; j++){
-                delete[] tex[i][j];
-            }
-            delete[] tex[i];
-        }
-        delete[] tex;
-
-        delete[] h;
-        delete[] w;
-    }
-};
 
 struct light{
     vertex Direction,
--- src/myTexture.h
+++ src/myTexture.h
@@ -1,6 +1,30 @@
 #ifndef MYTEXTURE_H_INCLUDED
 #define MYTEXTURE_H_INCLUDED
 
+#include <unistd.h>
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
 
+class myTexture{
+private:
+    uint32_t ***tex;
+    int *w, *h,
+        nb_mipmap = 1;
+
+public:
+    void init(std::string file_name, SDL_Window *screen);
+
+    int getMipMap();
+
+    int getH(int i);
+
+    int getW(int i);
+
+    uint32_t getTex(int mm, int w, int h);
+
+    ~myTexture();
+};
 
 #endif // MYTEXTURE_H_INCLUDED
